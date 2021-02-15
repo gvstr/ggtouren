3de734dd4105655a32f903ccc944463fa943e26d
unknown <gustav.strid@gmail.com>
2021-02-15 22:45:35 +0100

    more work on table

--- src/App.svelte
+++ src/App.svelte
@@ -5,20 +5,22 @@
   let rowIds = [];
 
   onMount(async () => {
-    console.log("on mount");
     let data = await fetch(`/api/tour`).then((res) => res.json());
     tours = data.data;
-    console.log(tours);
-    tours.forEach((x) => rowIds.push({ id: x._id, hidden: true }));
-    tours.forEach((year) => {
-      year.competitions.forEach((x) =>
-        rowIds.push({ id: x._id, hidden: true })
-      );
+    console.log(tours)
+    tours.forEach((tour) => {
+      let temp = [];
+      temp.push({ id: tour._id, hidden: true })
+        tour.competitions.forEach(competition => {
+          temp.push({ id: competition._id, hidden: true })
+        })
+        rowIds.push(temp);
     });
   });
 
-  let toggleId = (id) => {
-    rowIds.forEach((x) => {
+  let toggleId = (index, id) => {
+    console.log("toggle id: "+id)
+    rowIds[index].forEach((x) => {
       if (x.id === id) {
         x.hidden = !x.hidden;
       }
@@ -32,12 +34,30 @@
   {#if tours}
     {#each tours as tour, tourIndex}
       <!-- TOP MOST OBJECT -->
-      <span id="tour-header" on:click={() => toggleId(tour._id)}>{tour.name}</span>
+      <span id="tour-header" on:click={() => toggleId(tourIndex, rowIds[tourIndex][tourIndex].id)}>{tour.name}</span>
       <!-- Sub header starts here -->
-      <span class:hidden={rowIds[tourIndex].hidden} id="competition-header">Plats</span>
-      <span class:hidden={rowIds[tourIndex].hidden} id="competition-header">Datum</span>
-      <span class:hidden={rowIds[tourIndex].hidden} id="competition-header">Status</span>
-      <span class:hidden={rowIds[tourIndex].hidden} id="competition-header">Starttid</span>
+      <span class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-header">Plats</span>
+      <span class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-header">Datum</span>
+      <span class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-header">Status</span>
+      <span class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-header">Starttid</span>
+      {#each tour.competitions as competition, competitionIndex}
+      <!-- Competition rows start here -->
+        <span on:click={() => toggleId(tourIndex, rowIds[tourIndex][competitionIndex+1].id)} class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-row">{competition.location}</span>
+        <span on:click={() => toggleId(tourIndex, rowIds[tourIndex][competitionIndex+1].id)} class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-row">{competition.date}</span>
+        <span on:click={() => toggleId(tourIndex, rowIds[tourIndex][competitionIndex+1].id)} class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-row">{competition.status}</span>
+        <span on:click={() => toggleId(tourIndex, rowIds[tourIndex][competitionIndex+1].id)} class:hidden={rowIds[tourIndex][tourIndex].hidden} id="competition-row">{competition.starttime}</span>
+        <!-- Individual competition header starts here -->
+        <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">Namn</span>
+        <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">Poäng</span>
+        <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">Extrapoäng</span>
+        <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">Birdies</span>
+        {#each competition.players as player}
+          <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">{player.name}</span>
+          <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">{player.points}</span>
+          <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">{player.extraPoints}</span>
+          <span class:hidden={rowIds[tourIndex][competitionIndex+1].hidden} id="player-row">{player.birdies}</span>
+        {/each}
+      {/each}
     {/each}
   {:else}
     <h1>Nothing here</h1>
@@ -48,20 +68,33 @@
   .grid {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    border-top: 1px solid black;
-    border-right: 1px solid black;
     grid-template-areas: "header header header header";
   }
 
   .grid > span {
-    padding: 8px 4px;
-    border-left: 1px solid black;
-    border-bottom: 1px solid black;
+    padding: 4px 2px;
   }
 
   #tour-header {
     grid-area: header;
     text-align: center;
+    background-color: #274C77;
+    color: #E7ECEF;
+  }
+  #competition-header{
+    background-color: #478978;
+    color: #E7ECEF;
+    border-bottom: 1px solid black;
+  }
+  #competition-row{
+    background-color: #6596CD;
+    color: #E7ECEF;
+    border-bottom: 1px solid black;
+  }
+  #competition-row{
+    background-color: #93B5DC;
+    color: #E7ECEF;
+    border-bottom: 1px solid black;
   }
   .hidden {
     display: none;
